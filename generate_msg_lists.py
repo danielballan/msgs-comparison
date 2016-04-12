59e6bbb1f3efc7bf8e09a57e4216a8b3cfee8880
danielballan <daniel.b.allan@gmail.com>
2016-04-12 10:52:18 -0400

    Add generation script to repo

---
+++ generate_msg_lists.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from bluesky.examples import det, motor, motor1, motor2, MockFlyer, det1, det2
+from bluesky.plans import (Count, AbsScanPlan, AbsListScanPlan, DeltaListScanPlan,
+                           InnerProductAbsScanPlan, OuterProductAbsScanPlan)
+
+
+count_with_flyers = Count([det])
+count_with_flyers.flyers = [MockFlyer(det, motor)]
+
+plans = {'count-one-det': 'Count([det])',
+         'count-two-dets': 'Count([det1, det2], 2)',
+         'count-with-flyers': 'count_with_flyers',
+         'ascan': 'AbsScanPlan([det], motor, 1, 5, 3)',
+         'list-scan': 'AbsListScanPlan([det], motor, [1, 3, 8])',
+         'dscan-from-8': 'DeltaListScanPlan([det], motor, [1, 3, 8])',
+         'inner': 'InnerProductAbsScanPlan([det], 2, motor1, 1, 2, motor2, 10, 20)',
+         'outer': 'OuterProductAbsScanPlan([det], motor1, 1, 2, 2, motor2, 10, 20, 2, '
+                                  'False)'}
+
+
+def main(path):
+    for name, plan in plans.items():
+        with open(os.path.join(path, name), 'w') as f:
+            f.write(plan + "\n")
+            for msg in list(eval(plan)):
+                f.write(repr(msg) + "\n")
+
+    
+if __name__ == '__main__':
+    main(sys.argv[1])
